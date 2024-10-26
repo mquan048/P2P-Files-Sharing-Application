4ee6be6cd9798579752fc380c4cdb8453d0c8464
mquan048 <mquando04@gmail.com>
2024-10-26 18:43:15 +0700

    update and refactor code to OOP style

--- client.py
+++ client.py
@@ -1,172 +1,381 @@
 import socket
 import threading
 import os
+import pickle
+import tqdm
 from pathlib import Path
+from math import ceil
 
-def send(socket, message):
-    socket.send(message.encode())
-    check = socket.recv(BUFSIZE).decode()
-    if check != "OK":
-        raise Exception('Have error when send message')
+SERVER_HOST = '127.0.0.1'  
+SERVER_PORT = 8000        
+BUFSIZE = 1024
+CHUNK_SIZE = 512
+
+class Server:
+    server: socket.socket
+    list_peers_addr: list[(str, int)]
+    user = None
+    
+    def __init__(self):
+        self.server_addr = (SERVER_HOST, SERVER_PORT)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-def receive(socket):
-    message = socket.recv(BUFSIZE).decode()
-    socket.send("OK".encode())
-    return message
-
-def login(server):
-    check = False
-    while not check:
-        username = input('username: ')
-        password = input('password: ')
+        self.list_peers_addr = list[tuple[str, int]]()
         
-        send(server, username)
-        send(server, password)
-    
-        msg = receive(server)
-        print(msg)
         
-        if msg != 'Login fail':
-            check = True
-
-def sendListFile(server, uploadServer):
-    uploadServerHost, uploadServerPort = uploadServer.getsockname()
-    
-    send(server, uploadServerHost)
-    send(server, str(uploadServerPort))
-    
-    files = os.listdir(f'{Path(__file__).parent}\\files')
-    for file in files:
-        send(server, file)
-    send(server, "<END>")
-    
-def initUploadServer():
-    peerServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    peerServer.bind(('127.0.0.1', 0))
-    print('Upload server is running on %s:%s' % peerServer.getsockname())
-    
-    return peerServer
-    
-def runUploadServer():
-    uploadServer.listen(5)
-    try: 
-        while True:
-            peer, peerAddress = uploadServer.accept()
-            print('%s:%s has connected.' % peerAddress)
-            peerThread = threading.Thread(target=handleUpload, args=(peer,))
-            peerThread.start()
-             
-    except:
-        print('Upload server close')
-
-def handleUpload(peer):
-    fileName = receive(peer)
-    orderUpload = int(receive(peer))
-    totalUploader = int(receive(peer))
-    
-    file = open(f'{Path(__file__).parent}/files/{fileName}', 'rb')
-    fileSize = os.path.getsize(f'{Path(__file__).parent}/files/{fileName}')
-    data = file.read()
-    
-    startRead = (fileSize // totalUploader) * orderUpload
-    endRead = fileSize if (fileSize // totalUploader) * (orderUpload + 2) >= fileSize else  (fileSize // totalUploader) * (orderUpload + 1)
-     
-    dataSend = data[startRead : endRead]
-    
-    peer.sendall(dataSend)
-    peer.send(b'<END>')
-    
-    print(f"Close connection with {peer.getpeername()}")
-    peer.close()
-
-def handleDownload(fileName, listUploadServer, totalUploader):
-    listData = [b""] * totalUploader
-    listThread = []
-    for i in range(0, totalUploader):
-        downloadThread = threading.Thread(target=(handleDownloadThread), args=(fileName, i, totalUploader, listUploadServer[i], listData))
-        downloadThread.start()
-        listThread.append(downloadThread)
+    def __del__(self):
+        self.server.close()
         
-    for thread in listThread:
-        thread.join()
-    
-    file = open(f'{Path(__file__).parent}/files/{fileName}', 'wb')
-    
-    for data in listData:
-        file.write(data)
-    
-    file.close()
-    
-def handleDownloadThread(fileName, orderUpload, totalUploader, uploadAddress, listData):
-    uploadServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(f'Connecting to upload server {uploadAddress[0]}:{uploadAddress[1]}')
-    uploadServer.connect((uploadAddress[0],int(uploadAddress[1])))
-
-    send(uploadServer, fileName)
-    send(uploadServer, str(orderUpload))
-    send(uploadServer, str(totalUploader))
-    
-    fileData = b""
-    
-    while True:
-        data = uploadServer.recv(BUFSIZE)
-        fileData += data
-        if fileData[-5:] == b"<END>":
-            break
+    def run(self):
+        self.server.connect(self.server_addr)
+        print(f'Connecting to server {self.server_addr[0]}:{self.server_addr[1]}')
         
-    listData[orderUpload] = fileData[0:-5]
+    def login(self, username, password, peer_addr):
+        request = {
+            "type": "request",
+            "action": "login",
+            "payload": {
+                "username": username,
+                "password": password,
+                "peer_addr": peer_addr,
+            },
+        }
+        self.server.send(pickle.dumps(request))
+        
+        response = pickle.loads(self.server.recv(BUFSIZE))
+        if response["status"] == 200:
+            self.user = username
+        print(response["payload"])
 
-    print(f"Close connection with {uploadServer.getpeername()}")
-    uploadServer.close()
-   
-HOST = '127.0.0.1'  
-PORT = 8000    
-BUFSIZE = 1024    
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print(f'Connecting to server {HOST}:{PORT}')
-server.connect((HOST, PORT))
-
-uploadServer = initUploadServer()
-
-if __name__ == '__main__':
-    try:
-        checkQuit = False
-        while not checkQuit:
-            login(server)
-            uploadServerThread = threading.Thread(target=runUploadServer)
-            uploadServerThread.start()
-            sendListFile(server, uploadServer)
+    def logout(self):
+        request = {
+            "type": "request",
+            "user": self.user,
+            "action": "logout",
+        }
+        self.server.send(pickle.dumps(request))
+        
+        response = pickle.loads(self.server.recv(BUFSIZE))
+        print(response["payload"])
+        
+        self.user = None
+    
+    def get_peer_info(self):
+        request = {
+            "type": "request",
+            "user": self.user,
+            "action": "get_peer_info",
+        }   
+        self.server.send(pickle.dumps(request))
+    
+        response = pickle.loads(self.server.recv(BUFSIZE))
+        if response["status"] == 401:
+            print(response["payload"])
             
-            while True: 
-                action = input()
-                send(server, action)
+        else:
+            self.list_peers_addr = response["payload"]
+    
+class Peer:
+    peer: socket.socket
+    available_files: dict[(str, int), set[(str, int)]] # dict[(filename, size), set[peer_addr]]
+    server: Server
+    
+    def __init__(self, server: Server):
+        self.peer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.peer.bind(('127.0.0.1', 0))
+        
+        self.available_files = dict[tuple[str, int], set[tuple[str, int]]]()
+        self.server = server
+        
+    def __del__(self):
+        self.peer.close()
+    
+    def run(self):
+        self.peer.listen(10)
+        self.accept_thread = threading.Thread(target=self.accept_connection)
+        self.accept_thread.start()
 
-                if action == 'DOWNLOAD':
-                    fileName = input('File need to download: ')
-                    send(server, fileName)
-                    
-                    totalUploader = int(receive(server))
-                    listUploadServer = []
-                    
-                    for i in range(0, totalUploader):
-                        uploadServerHost = receive(server)
-                        uploadServerPort = receive(server)
-                        listUploadServer.append((uploadServerHost, uploadServerPort))
-                    
-                    downloadThread = threading.Thread(target=handleDownload, args=(fileName, listUploadServer, totalUploader))
-                    downloadThread.start()
-                    
-                elif action == "LOGOUT":
-                    msg = receive(server)
-                    print(msg)
-                    break
+    def accept_connection(self):
+        while True:
+            peer, peer_addr = self.peer.accept()
+            peer_thread = threading.Thread(target=self.listen_peer, args=(peer,))
+            peer_thread.start()
+            
+    def listen_peer(self, peer: socket.socket):
+        while True:
+            resquest = pickle.loads(peer.recv(BUFSIZE))
+            if resquest["action"] == "fetch_file":
+                payload = resquest["payload"]
                 
-                elif action == "QUIT":
-                    msg = receive(server)
-                    print(msg)
-                    checkQuit = True
-                    break  
+                file = Read_File(payload["filename"], payload["size"])
+                data = file.read(payload["chunk_no"])
                 
-    finally:
-        uploadServer.close()
-        server.close()
+                response = {
+                    "type": "response",
+                    "status": 200,
+                    "action": "fetch_file",
+                    "payload": {
+                        "filename": payload["filename"],
+                        "size": payload["size"],
+                        "chunk_no": payload["chunk_no"],
+                        "data": data
+                    },
+                }
+                peer.send(pickle.dumps(response))
+                
+                del file
+            
+            elif resquest["action"] == "get_shared_file":
+                list_files_res = list[tuple[str, int]]()
+                
+                path_folder = Path.joinpath(Path(__file__).parent, "shared_files")
+                files = os.listdir(path_folder)
+                for file in files:
+                    fileSize = os.path.getsize(Path.joinpath(path_folder, file))
+                    list_files_res.append((file, fileSize))
+                    
+                response = {
+                    "type": "response",
+                    "status": 200,
+                    "action": "get_shared_file",
+                    "payload": list_files_res,
+                }
+                peer.send(pickle.dumps(response))
+                
+                
+            elif resquest["action"] == "close_conn":
+                peer.close()
+                break
+
+    def get_available_files(self):
+        
+        running_thread = list[threading.Thread]()
+        new_available_files = dict[tuple[str, int], set[tuple[str, int]]]()
+        
+        for peer_addr in self.server.list_peers_addr:
+            if peer_addr == self.peer.getsockname():
+                continue
+            
+            peer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            peer.connect(peer_addr)
+            
+            peer_thread = threading.Thread(target=self.get_shared_file, args=(peer, new_available_files))
+            peer_thread.start()
+            running_thread.append(peer_thread)
+            
+        for thread in running_thread:
+            thread.join()
+            
+        self.available_files = new_available_files
+            
+    def get_shared_file(self, peer: socket.socket, new_available_files: dict[tuple[str, int], set[tuple[str, int]]]):
+        resquest = {
+            "type": "request",
+            "action": "get_shared_file",
+        }
+        peer.send(pickle.dumps(resquest))
+        response = pickle.loads(peer.recv(BUFSIZE))
+        for file in response["payload"]:
+            if file in new_available_files:
+                new_available_files[file].add(peer.getpeername())
+            else:
+                new_available_files[file] = set([peer.getpeername()])
+                
+        peer.send(pickle.dumps({
+            "type": "request",
+            "action": "close_conn",
+        }))
+        peer.close()
+
+class Read_File:
+    filename: str
+    size: int
+    path_folder: str
+    
+    chunk_size = CHUNK_SIZE
+    
+    def __init__(self, filename, size):
+        self.filename = filename
+        self.size = size
+        self.path_folder = Path.joinpath(Path(__file__).parent, "shared_files")
+        
+    def read(self, chunk_no):
+        file = open(Path.joinpath(self.path_folder, self.filename), "rb")
+        file.seek(chunk_no * self.chunk_size, 0)
+        data = file.read(self.chunk_size)
+        return data
+           
+class Fetch_File:
+    filename: str
+    size: int
+    path_folder: str
+    
+    total_chunk: int
+    current_chunk: int
+    chunk_data: list[str]
+    
+    peers: list[socket.socket]
+    
+    chunk_size = CHUNK_SIZE
+        
+    def __init__(self, filename, size, available_files):
+        self.filename = filename
+        self.size = size
+        self.path_folder = Path.joinpath(Path(__file__).parent, "shared_files")
+        
+        self.init_chunk()
+        
+        self.peers = list[socket.socket]()
+        
+        if (filename, size) in available_files:
+            peers_addr = available_files[(filename, size)]
+        else:
+            peers_addr = set[tuple[str, int]]()
+            
+        for peer_addr in peers_addr:
+            p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            p.connect(peer_addr)
+            self.peers.append(p)
+        
+    def __del__(self):
+        for peer in self.peers:
+            peer.close()
+    
+    def init_chunk(self):
+        self.total_chunk = ceil(self.size / self.chunk_size)
+        self.current_chunk = 0
+        self.chunk_data = [b""] * self.total_chunk
+        
+        self.is_access_cur_chunk = False
+        
+    def get_chunk_no(self) -> int:
+    # Handling the race condition of variable current_chunk.
+        while self.is_access_cur_chunk:
+            pass
+        
+        self.is_access_cur_chunk = True
+        
+        chunk_no = self.current_chunk
+        if self.current_chunk < self.total_chunk:
+            self.current_chunk += 1
+        
+        self.is_access_cur_chunk = False
+            
+        return chunk_no
+    
+    def fetch_file(self):
+        if not self.peers:
+            print("File not found.")
+            return
+        
+        running_thread = list[threading.Thread]()
+        progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=self.size, desc=self.filename)
+        
+        for peer in self.peers:
+            fetch_thread = threading.Thread(target=self.fetch_chunk, args=(peer, progress,))
+            fetch_thread.start()
+            running_thread.append(fetch_thread)
+            
+        for fetch_thread in running_thread:
+            fetch_thread.join()
+            
+        file = open(Path.joinpath(self.path_folder, self.filename), "wb")
+        
+        for data in self.chunk_data:
+            file.write(data)
+            
+        file.close()
+        
+        # print(f"Fetch file {self.filename} successfully.")
+    
+    def fetch_chunk(self, peer: socket.socket, progress: tqdm.tqdm):
+        while True:
+            chunk_no = self.get_chunk_no()
+            
+            if chunk_no < self.total_chunk:
+                resquest = {
+                    "type": "request",
+                    "action": "fetch_file",
+                    "payload": {
+                        "filename": self.filename,
+                        "size": self.size,
+                        "chunk_no": chunk_no
+                    },
+                }
+                peer.send(pickle.dumps(resquest))
+                
+                response = pickle.loads(peer.recv(BUFSIZE))
+                
+                data = response["payload"]["data"]
+                self.chunk_data[chunk_no] = data
+                
+                progress.update(BUFSIZE)
+                
+            else:
+                resquest = {
+                    "type": "request",
+                    "action": "close_conn",
+                }
+                peer.send(pickle.dumps(resquest))
+                peer.close()
+                break                           
+
+if __name__ == "__main__":
+    try:
+        server = Server()
+        server.run()
+        peer = Peer(server) 
+        peer.run()
+       
+        print("Give one of the commands:")
+        print("1|login: Log in to server")
+        print("2|get_peer: Get list of peers in network")
+        print("3|get_available_files: Get the list of available files in network")
+        print("4|fetch_file: Download file from peers in network")
+        print("5|logout: Log out from server")
+        print("6|close: Close connection with server and exit program\n\n")
+        
+        while True:
+            inp = input(">>")
+            if inp == "login" or inp == "1":
+                if server.user is None:
+                    username = input("username: ")
+                    password = input("password: ")
+                    
+                    server.login(username, password, peer.peer.getsockname())
+                    
+                else:
+                    print("You are already logged in, so cannot log in again.")
+             
+            elif inp == "get_peer" or inp == "2":
+                server.get_peer_info()
+                print(server.list_peers_addr)
+                    
+            elif inp == "get_available_files" or inp == "3":
+                server.get_peer_info()
+                peer.get_available_files()
+                print(peer.available_files)
+                
+            elif inp == "fetch_file" or inp == "4": 
+                server.get_peer_info()
+                peer.get_available_files()
+                  
+                filename = input("filename: ")
+                size = int(input("size: "))
+                
+                file = Fetch_File(filename, size, peer.available_files)
+                file.fetch_file()
+                del file
+                
+            elif inp == "logout" or inp == "5":
+                if server.user is None:
+                    print("You are not logged in, so cannot log out.")
+                    
+                else:
+                    server.logout()
+                    
+            elif inp == "close" or inp == "6":
+                os._exit(0)
+                
+    except KeyboardInterrupt:
+        os._exit(0)
--- server.py
+++ server.py
@@ -1,138 +1,141 @@
 import socket
 import threading
 import json
+import os
+import pickle
 from pathlib import Path
-
-def send(socket, message):
-    socket.send(message.encode())
-    check = socket.recv(BUFSIZE).decode()
-    if check != "OK":
-        raise Exception('Have error when send message')
-        
-def receive(socket):
-    message = socket.recv(BUFSIZE).decode()
-    socket.send("OK".encode())
-    return message
-
-def validClient(username, password):
-    for acc in listClients:
-        if acc[0] == username:
-            return False
-    with open(f'{Path(__file__).parent}/account.json', 'r') as db:
-        accountList = json.load(db)
-    for acc in accountList:
-        if acc['username'] == username and acc['password'] == str(password):
-            return True
-    return False
-
-def login(client):
-    while True:
-        username = receive(client)
-        password = receive(client)
-        
-        if validClient(username, password):
-            msg = f"username {username} login successfully"
-            send(client, msg)
-            print(msg)
-            
-            return username
-        else:
-            send(client, "Login fail")  
-
-def logout(account):
-    accRemove = None
-    for acc in listClients:
-        if acc[0] == account:
-            accRemove = acc
-    for file in listFiles.copy():
-        if file[1] == accRemove[2]:
-            listFiles.remove(file)
-    listClients.remove(accRemove)    
-    
-def updateListFiles(client):
-    uploadHost = receive(client)
-    uploadPort = receive(client)
-    
-    while True:
-        fileName = receive(client)
-        if fileName == "<END>":
-            break
-        listFiles.append((fileName, (uploadHost, uploadPort)))
-        
-    print(listFiles)
-    return (uploadHost, uploadPort)
-
-def handleDownload(client):
-    fileName = receive(client)
-    listPeer = []
-    for file in listFiles:
-        if file[0] == fileName:
-            listPeer.append((file[1][0], file[1][1]))
-            
-    # send number of peer have this file
-    send(client, str(len(listPeer)))
-    
-    for peer in listPeer:
-        send(client, peer[0])
-        send(client, peer[1])
-
-def handleClient(client, clientAddress):
-    global listAccount
-    global listAddress
-    try:
-        checkQuit = False
-        account = None
-        while not checkQuit:
-            account = login(client)
-            uploadAddress = updateListFiles(client)
-            listClients.append((account, clientAddress, uploadAddress))
-            print(listClients)
-            
-            while True:
-                action = receive(client)
-                print(f"{clientAddress[0]}:{clientAddress[1]}: {action}")
-                
-                if action == "DOWNLOAD":
-                    handleDownload(client)
-                    
-                elif action == "LOGOUT":
-                    logout(account)
-                    account = None
-                    send(client, "Logout successfully")
-                    break
-                
-                elif action == "QUIT":
-                    logout(account)
-                    account = None
-                    send(client, "Logout successfully")
-                    checkQuit = True
-                    break
-    except:
-        if not account is None:
-            logout(account)
-    finally:
-        print(f"Close connection with {client.getpeername()}")
-        client.close()    
 
 HOST = '127.0.0.1'  
 PORT = 8000        
 BUFSIZE = 1024
 
-listClients = []
-listFiles = []
-
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-
-if __name__ == '__main__':
-    server.listen(5)
-    print(f'Server is running on {HOST}:{PORT}')
-    try:
+class Server:
+    server: socket.socket
+    connections: dict[str, tuple[socket.socket, (str, int)]]  # dict[username, (socket, peer_addr)]
+    
+    def __init__(self):
+        self.addr = (HOST, PORT)
+        self.connections = dict[str, tuple[socket.socket, tuple[str, int]]]()
+        
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(self.addr)
+        self.server.listen(10)
+        
+        print(f"Server is running on {self.addr[0]}:{self.addr[1]}")
+        
+    def __del__(self):
+        self.server.close()
+    
+    def run(self):
+        self.accept_thread = threading.Thread(target=self.accept_connection)
+        self.accept_thread.start()   
+                
+    def accept_connection(self):
         while True:
-            client, clientAddress = server.accept()
-            print('%s:%s has connected.' % clientAddress)
-            clientThread = threading.Thread(target=handleClient, args=(client, clientAddress))
-            clientThread.start()
-    finally:
-        server.close()
+            client, client_addr = self.server.accept()
+            print(f"{client_addr[0]}:{client_addr[1]} has connected.")
+            client_thread = threading.Thread(target=self.recv_req, args=(client,))
+            client_thread.start()
+    
+    def recv_req(self, client: socket.socket):
+        while True:
+            try:
+                request = pickle.loads(client.recv(BUFSIZE))
+                if request["action"] == "login":
+                    self.login(request["payload"], client)
+                    
+                elif request["action"] == "logout":
+                    self.logout(request["user"], client)
+                
+                elif request["action"] == "get_peer_info":
+                    self.send_peer_info(request, client)
+                
+                elif request["action"] == "close_conn":
+                    client.close()
+            except:
+                self.handle_suddenly_exit(client)
+                client.close()
+                break
+    
+    def login(self, payload, client: socket.socket):
+        username = payload["username"]
+        password = payload["password"]
+
+        response = {
+            "type": "respone",
+            "action": "login",
+        }
+        
+        # check username is logged in
+        if username in self.connections:
+            response["status"] = 400
+            response["payload"] = "The user is already logged in on another device."
+            
+            client.send(pickle.dumps(response))
+            return
+         
+        with open(f'{Path(__file__).parent}/account.json', 'r') as db:
+            account = json.load(db)
+
+        for acc in account:
+            if username == acc["username"] and password == acc["password"]:
+                self.connections[username] = (client, payload["peer_addr"])
+                
+                response["status"] = 200
+                response["payload"] = f"User {username} log in successfully."
+                
+                client.send(pickle.dumps(response))
+                return
+            
+        response["status"] = 400
+        response["payload"] = "The username or password is incorrect."
+              
+        client.send(pickle.dumps(response))
+       
+    def logout(self, username: str, client: socket.socket):
+        del self.connections[username]
+            
+        response = {
+            "type": "respone",
+            "status": 200,
+            "action": "logout",
+            "payload": "User log out successfully."
+        }
+        client.send(pickle.dumps(response))           
+             
+    def send_peer_info(self, request, client: socket.socket):
+        response = {
+                "type": "respone",
+                "action": "get_peer_info",
+            }
+        if request["user"] is None or not request["user"] in self.connections:
+            response["status"] = 401
+            response["payload"] = "The user is not authenticated."
+            
+        else:
+            peer_info = list()
+            for conn in self.connections.values():
+                peer_info.append(conn[1])
+            response["status"] = 200
+            response["payload"] = peer_info
+            
+        client.send(pickle.dumps(response))
+      
+    def handle_suddenly_exit(self, client: socket.socket):
+        connections = self.connections.copy()
+        for conn in self.connections:
+            if self.connections[conn][0] == client:
+                del connections[conn]
+        self.connections = connections
+
+if __name__ == "__main__":
+    try: 
+        server = Server()
+        server.run()
+        inp = input()
+        if inp == "close":
+            os._exit(0)
+
+    except KeyboardInterrupt:
+        os._exit(0)
+        
